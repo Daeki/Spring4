96a07f920f11e46ae8eadb7a21016e02f9fd1f8d
Daeki <kimdaeki@gmail.com>
2021-09-08 16:52:33 +0900

    1

--- src/main/webapp/resources/js/boardCheck.js
+++ src/main/webapp/resources/js/boardCheck.js
@@ -5,5 +5,44 @@
  const btn = document.getElementById("btn");
  
  btn.addEventListener("click", function(){
-	alert('click');
+	const title = document.getElementById("title");
+	const writer = document.getElementById("writer");
+	const frm = document.getElementById("frm");
+	const t1 = document.getElementById("t_1");
+	const w1 = document.getElementById("w_1");
+	t1.innerHTML="";
+	w1.innerHTML="";
+	
+	let t = title.value;
+	let w = writer.value;
+	
+	let check=true;
+	
+	if(t == ""){
+		check=false;
+		t1.innerHTML="필수 입력";
+	}
+	
+	if(w == ""){
+		check=false;
+		w1.innerHTML="필수 입력";
+	}
+	
+	if(check){
+		frm.submit();
+	}else {
+		alert('필수 입력...');
+	}
+
+	//1
+/*	if(t != "" && w != ""){
+		alert(true);
+	}else {
+		alert(false);
+	}*/
+	
+
+	
+	
+	
 });
