b09eedfacc27c72f4d817dbdcf3a92f564d071f7
Daeki <kimdaeki@gmail.com>
2021-09-16 12:17:00 +0900

    1

--- src/main/java/com/iu/s4/board/notice/NoticeDTO.java
+++ src/main/java/com/iu/s4/board/notice/NoticeDTO.java
@@ -1,8 +1,23 @@
 package com.iu.s4.board.notice;
 
+import java.util.List;
+
 import com.iu.s4.board.BoardDTO;
+import com.iu.s4.board.BoardFilesDTO;
 
 public class NoticeDTO extends BoardDTO {
 	// is a
 	// has a
+	private List<BoardFilesDTO> files;
+
+	public List<BoardFilesDTO> getFiles() {
+		return files;
+	}
+
+	public void setFiles(List<BoardFilesDTO> files) {
+		this.files = files;
+	}
+	
+	
+	
 }
